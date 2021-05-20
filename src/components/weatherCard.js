2d985f5dd0c3924cbd45d4726fca0797df1be666
ShirazMuhd <shirazmuhdofficial@gmail.com>
2021-05-20 18:31:23 +0530

    description added

--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import './weatherCard.css'
+import React from "react";
+import "./weatherCard.css";
 
 function WeatherCard(props) {
-    return (
-        <div className="weatherCard-container">
-            <div className="weatherCard">
-                <div className="country">
-                    <h3><span>Boston</span><sup>am</sup></h3>
-                </div>
-                <div className="temp">
-                    <h1>23&deg;<sup>c</sup></h1>
-                </div>
-            </div>
+  return (
+    <div className="weatherCard-container">
+      <div className="weatherCard">
+        <div className="country">
+          <h3>
+            <span>Boston</span>
+            <sup>am</sup>
+          </h3>
         </div>
-    )
+        <div className="temp">
+          <h1>
+            23&deg;
+            <sup>C</sup>
+          </h1>
+        </div>
+        <div className="description">
+            <img src="" alt="" />
+            <h4>Clear sky</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WeatherCard;
